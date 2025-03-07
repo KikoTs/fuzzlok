33130e5bb7898d5d7e8ccb3dabdd9774d8f21a1f
Will Tomlinson <watomlinson@gmail.com>
2025-03-06 23:21:01 -0500

    Fuzzer either works or it's shit, but it was interesting anyway

--- saflok.py
+++ saflok.py
@@ -1,8 +1,4 @@
-# Encrypted Card data
-strCard = [
-    0x64, 0xC0, 0x6F, 0x56, 0x13, 0x69, 0x20, 0xDB, 0x8B, 0x04, 0xE9, 0x32, 0x34, 0x0F, 0xA4, 0xA0,
-    0x8A
-]
+from random import randint
 
 # Some sort of static encryption key??
 c_aDecode = [
@@ -45,53 +41,135 @@ key_levels = [
     (16, "Primary Programming Key (PPK)"),
 ]
 
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-# Where we put the decrypted card data
-decryptedCard = []
+bytes_to_change = 2
+iter_count = 0
 
-# Setup
-length = 17
-b = 0
-b2 = 0
+while True:
 
-# Stage 1??
-for i in range(0, length):
-    num = c_aDecode[strCard[i]] - (i + 1)
-    if num < 0:
-        num += 256
-    decryptedCard.append(num)
+    # Encrypted Card data
+    strCard = [
+        0x64, 0xC0, 0x6F, 0x56, 0x13, 0x69, 0x20, 0xDB, 0x8B, 0x04, 0xE9, 0x32, 0x34, 0x0F, 0xA4, 0xA0,
+        0x8A
+    ]
 
-print("Stage1: {}".format(decryptedCard))
+    # Pick a random bytes to change (we may accidentally change the same ones)
+    for _ in range(0,bytes_to_change):
+        pos = randint(0,16)
+        b = randint(0,255)
+        strCard[pos] = b
+        # print("Changing strCard[{}] to 0x{:02x}".format(pos,b))
 
-# Stage 2
-b = decryptedCard[10]
-b2 = b & 1 #Even/odd thingy
+    # Where we put the decrypted card data
+    dc = []
 
-print("Stage2: b:{:02x} b2:{:02x}".format(b,b2))
+    # Setup
+    length = 17
 
-# Stage 3
-for num2 in range(17, 0, -1):
-    b = decryptedCard[num2 - 1]
-    print("Set b = decryptedCard[{}] --> 0x{:02x}".format(b, decryptedCard[num2 -1]))
-    for num3 in range(8,0,-1):
-        num4 = num2 + num3
-        if(num4 > length):
-            num4 -= length
-        b3 = decryptedCard[num4 - 1]
-        b4 = (b3 & 0x80) >> 7
-        b3 = ((b3 << 1) & 0xFF) | b2
-        b2 = (b & 0x80) >> 7
-        b =  ((b << 1) & 0xFF) | b4
-        decryptedCard[num4 - 1] = b3
-    decryptedCard[num2 - 1] = b
+    # Stage 1??
+    for i in range(0, length):
+        num = c_aDecode[strCard[i]] - (i + 1)
+        if num < 0:
+            num += 256
+        dc.append(num)
 
-pos = 0
-for byte in decryptedCard:
-    print("Byte {} -> 0x{:02x}".format(pos,byte))
-    pos += 1
+    # Stage 2
+    bob = dc[10]
+    alice = bob & 1 #Even/odd thingy
 
+    # Stage 3
+    for zeta in range(17, 0, -1):
+        bob = dc[zeta - 1]
+        for xi in range(8, 0, -1):
+            yip = zeta + xi
+            if(yip > length):
+                yip -= length
+            charlie = dc[yip - 1]
+            david = (charlie & 0x80) >> 7
+            charlie = ((charlie << 1) & 0xFF) | alice
+            alice = (bob & 0x80) >> 7
+            bob =  ((bob << 1) & 0xFF) | david
+            dc[yip - 1] = charlie
+        dc[zeta - 1] = bob
 
-## Print info ##
-key_level_no, key_level_text = key_levels[(decryptedCard[0] & 0xF0) >> 4]
+    # Print the contents of our decrypted array
+    pos = 0
+    for byte in dc:
+        pos += 1
 
-print("Key Level ({}): {}".format(key_level_no, key_level_text))
+    ## Print info ##
+    # Byte 0
+    key_level_no, key_level_text = key_levels[(dc[0] & 0xF0) >> 4]
+    led_warning = (dc[0] & 0x08) >> 3
+
+    # Byte 1
+    key_id = dc[1]
+
+    # Byte 2 & 3
+    key_record_high = dc[2] & 0x7F
+    opening_key = (dc[2] & 0x80) >> 7
+    key_record = (key_record_high << 8) | dc[3]
+
+    # Byte 5 & 6
+    sequence_combination_number = ((dc[5] & 0x0F) << 8) | dc[6]
+
+    # Property ID and year, bytes 14 & 15
+    creation_year_bits = (dc[14] & 0xF0)
+    property_id = ((dc[14] & 0x0F) << 8) | dc[15]
+
+    # Byte 7 override deadbolt and days
+    override_deadbolt = (dc[7] & 0x80) >> 7
+    restricted_weekday = dc[7] & 0x7F
+
+    interval_year = dc[8] >> 4
+    interval_month = dc[8] & 0x0f
+    interval_day = (dc[9] >> 3) & 0x1F
+    interval_hour = ((dc[9] & 0x07) << 2) | (dc[10] >> 6)
+    interval_minute = dc[10] & 0x3F
+
+    creation_year = (((dc[11] & 0xF0) >> 4) + 1980) | creation_year_bits
+    creation_month = dc[11] & 0x0F;
+    creation_day = (dc[12] >> 3) & 0x1F;
+    creation_hour = ((dc[12] & 0x07) << 2) | (dc[13] >> 6);
+    creation_minute = dc[13] & 0x3F;
+
+    checksum = dc[16]
+
+    # Let's validate the checksum
+    csum = 0
+
+    # Duh, checksum isn't part of the checksum, hence 16 instead of 17
+    for i in range(0, 16):
+        csum += dc[i]
+
+    csum = 255 - (csum & 0xff)
+
+    csum_pass = checksum == csum
+
+    # print("Iteration {}".format(iter_count))
+    iter_count += 1
+
+    if interval_year > 1 and csum_pass and opening_key == 1 and property_id == 1142 and override_deadbolt == 1:
+        for byte in strCard:
+            print("{:02x}".format(byte),end=" ")
+        print()
+        # Tell me about yourself
+        print("----KEY INFO----")
+        print("Key Level ({}): {}".format(key_level_no, key_level_text))
+        print("LED Warn: {}".format(led_warning))
+        print("Key ID: 0x{:02x}".format(key_id))
+        print("Opening key: {}".format(opening_key))
+        print("Key Record: {:04x}".format(key_record))
+        print("Seq. combination: 0x{:02x}".format(sequence_combination_number))
+        print("Creation yr. bits: {}".format(creation_year_bits))
+        print("Property ID: {}".format(property_id))
+        print("Override deadbolt: {}".format(override_deadbolt))
+        print("Restricted days (mtwtfss): {:07b}".format(restricted_weekday))
+        print("Valid for (YYYY/MM/DDThm): {:04}/{:02}/{:02}T{:02}:{:02}".format(interval_year, interval_month, interval_day, interval_hour, interval_minute))
+        print("Creation (YYYY/MM/DDThh:mm): {:04}/{:02}/{:02}T{:02}:{:02}".format(creation_year, creation_month, creation_day, creation_hour, creation_minute))
+        print("Checksum: 0x{:02x}".format(checksum))
+        print("Computed checksum: 0x{:02x}".format(csum))
+        print("Checksum pass: {}".format(csum_pass))
+        print("----END KEY INFO----")
+
